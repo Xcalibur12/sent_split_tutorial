9e7b6b1c960dd82f53d4ddb8699317197147cac3
Xcalibur12 <45345050+Xcalibur12@users.noreply.github.com>
2018-12-10 01:15:53 +0900

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -5,7 +5,7 @@ input_wav = ".\\data\\test.wav"
 trn_name = ".\\data\\test.trn"
 
 def main():
-    sent_list = silence_detector.sent_split(trn_name)
+    sent_list = sent_splitter.sent_split(trn_name)
     wave_splitter.wav_split(input_wav, sent_list)
 
 if __name__ == '__main__':
